abaa86c37d236198a3ee44f51065b452ae72a2a2
Rahmat Putra <radeputra@gmail.com>
2022-04-30 15:15:16 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        myPrimary: '#14b8a6',
+        myDark: '#0f172a',
+        mySecondary: '#64748b',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
